d9ba270f600b39ba1040f750f47b34bb2cd7186f
akhil5564 <vpakhil881@gmail.com>
2025-01-10 21:38:39 +0530

    removeunusedimports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';  // Import Login component
 import Home from './components/Home';    // Import Home component
--- src/pages/Reporter.tsx
+++ src/pages/Reporter.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 
 interface ReportProps {
     dataList: any[];
